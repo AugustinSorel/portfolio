1a025067974c0380b36dcb64513e5b1fd566d633
AugustinSorel <sorelaugustin@gmail.com>
2022-05-29 16:17:36 +0200

    pseudo element bug removed

--- client/components/UIElements/LanguageText/LanguageText.styled.ts
+++ client/components/UIElements/LanguageText/LanguageText.styled.ts
@@ -19,21 +19,21 @@ export const Text = styled(motion.h2)<Props>`
   position: relative;
   cursor: pointer;
 
-  &::before {
+  button::before {
     ${line}
     display: ${({ $isActive }) => ($isActive ? "block" : "none")};
     top: calc(50% - var(--border-height) / 2);
     width: 100%;
   }
 
-  &::after {
+  button::after {
     ${line}
     left: 0;
     width: 0;
     transition: all 0.3s ease-in-out;
   }
 
-  &:hover::after {
+  button:hover::after {
     width: 100%;
   }
 `;
