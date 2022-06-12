56f029a4aa8a58943c060d9d8f02b31404890d54
AugustinSorel <sorelaugustin@gmail.com>
2022-06-12 13:58:32 +0200

    cleaner code

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,6 @@ import express from "express";
 import apiError from "./middlewares/apiError.middleware";
 import router from "./routes";
 import cors from "cors";
-import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,6 +10,5 @@ app.use(cors({ origin: true }));
 app.use(express.json());
 app.use("/api", router);
 app.use(apiError);
-app.use("/api", express.static(path.join(__dirname, "../public")));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
