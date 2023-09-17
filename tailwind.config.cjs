bd6401684ab79ca92357175241b6467cf1785d9d
AugustinSorel <sorelaugustin@gmail.com>
2023-09-17 16:40:31 +0100

    dark mode added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,10 +7,20 @@ module.exports = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
 
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+        primary: "hsl(var(--primary))",
+        border: "hsl(var(--border))",
+
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
         },
+      },
+      padding: {
+        "max-width":
+          "max(calc((100vw - var(--max-width-container)) / 2), 1.25rem)",
+      },
+      fontFamily: {
+        hello: ["oleo script"],
       },
     },
   },
